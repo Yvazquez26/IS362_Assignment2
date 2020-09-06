b0b82b3d68044df5a2545ab51315e5115d1aa7bf
Yvazquez26 <57605375+Yvazquez26@users.noreply.github.com>
2020-09-06 19:13:28 -0400

    Add files via upload

---
+++ IS362-Week 2 Assignment.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Week 2 - One aspect of data structures that I find challenging are tuples, 
+# looping through tuples and checking if items exists I keep getting that
+# 'kiwi' is not in the new tuple.
+
+# This function joins two tuples together using the zip function.
+
+a = ("apple", "banana", "cherry")
+b = ("Kiwi", "orange", "mango")
+
+x = zip(a, b)
+
+print(tuple(x))
+
+
+# This function is to determine if a specified item is present in the new tuple.
+
+if "kiwi" in tuple(x):
+    print("Yes, 'apple' is in the x tuple")
+else:
+    print("No, 'Kiwi' is not in the x tuple")
+
+
+
+
